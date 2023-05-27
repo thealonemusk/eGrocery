2f37d11118b6fe175842963f1177b3b4ed03d368
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-05-27 20:46:41 +0530

    changed some minor stuff

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eCommerce.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eGrosery.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
